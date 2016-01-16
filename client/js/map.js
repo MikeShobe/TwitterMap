c0ccb11b81d6132f5e352cbb907af747d1be5cb3
MIKE SHOBE <mikeshobe@yahoo.com>
2016-01-15 21:23:30 -0800

    added request module & restructured a bit

--- client/js/map.js
+++ client/js/map.js
@@ -1,7 +1,7 @@
 var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8
+    center: {lat: 34.0500, lng: -118.2500},
+    zoom: 10
   });
 }
--- server/server.js
+++ server/server.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var app = express();
-//request?
+var request = require('request');
 // var twitterAPI = require('twitterAPI');
 
 app.use(express.static('client'));
 
 // app.get('/', function(req, res) {
-//   res.send(body);
+//   request.get('', 
+//     function(err, response, body) {
+//       res.send(body);
+//   });
 // });
 
 console.log('Server connected - localhost:3000');
