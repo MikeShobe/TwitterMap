06acdb80f609bc59b8cd3906a74b0ec2b6635353
MIKE SHOBE <mikeshobe@yahoo.com>
2016-02-05 20:43:57 -0800

    added chai and html test, just need test for tweets

--- test/test-server.js
+++ test/test-server.js
@@ -1,4 +1,5 @@
 var request = require('supertest');
+var expect = require('chai').expect;
 var server = require('../server/server.js');
 
 describe('server', function(){
@@ -8,13 +9,29 @@ describe('server', function(){
       .expect(200, done)
   });
 
-  // it('Responded with JSON', function(done){
+  it('Should have access to index.html', function(done){
+    request(server)
+      .get('/')
+      .set('Accept', 'text/html')
+      .expect(200)
+      .end(function(err, res){
+        expect(res).to.have.property('text');
+        expect(res.text).to.not.equal(null);
+        done();
+      })
+  });
+
+  // it('Should be accessing tweets from stream/socket', function(done){
   //   request(server)
   //     .get('/')
   //     .set('Accept', 'application/json')
   //     .expect(200)
-  //     console.log(request(server))
-  //     done()
+  //     .end(function(err, res){
+  //       console.log(res.text);
+  //       expect(res).to.have.property('text');
+  //       expect(res.text).to.not.equal(null);
+  //       done();
+  //     })
   // });
 
 });
